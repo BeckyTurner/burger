c75f217cdb2e5508eec6de5213c398c07309fafe
rebeccaturner39 <beckyturner3992@gmail.com>
2019-05-21 20:03:39 -0500

    mostly functional

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,38 @@
+//dependencies
+var express = require("express");
+var burger = require("../models/burger.js");
+var router = express.Router();
+
+//get all burgers from db
+router.get("/", function (req, res) {
+    burger.selectAll("burgers", function (data) {
+        var burgerDataObj = {
+            burgers: data
+        };
+        console.log(burgerDataObj);
+        res.render("index", burgerDataObj);
+    });
+});
+
+//add new burger
+router.post("/api/burgers", function (req, res) {
+    burger.insertOne("burger_name", [req.body.burger], function (err, result) {
+        if (err) {
+            console.log(err)
+        }
+        console.log("hitting post route")
+        res.redirect("/");
+    });
+});
+
+//update burgers when devoured-this route is not working
+router.put("/api/burgers/:id", function (req, res) {
+    var condition = "id = " + req.params.id;
+    console.log("condition", condition);
+    console.log(req.params.id);
+    burger.update({ devoured: req.body.devoured }, condition, function () {
+        res.redirect('/');
+    });
+});
+
+module.exports = router;
---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ("Cheeseburger");
+INSERT INTO burgers (burger_name) VALUES ("Double Cheeseburger");
+INSERT INTO burgers (burger_name) VALUES ("Bacon Cheeseburger");
+INSERT INTO burgers (burger_name) VALUES ("California Cheeseburger");
