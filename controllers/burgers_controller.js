803f4acf41ef4e5583461f4382a8ba86c99148b6
rebeccaturner39 <beckyturner3992@gmail.com>
2019-05-21 20:27:42 -0500

    asd

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -26,13 +26,13 @@ router.post("/api/burgers", function (req, res) {
 });
 
 //update burgers when devoured-this route is not working
-router.put("/api/burgers/:id", function (req, res) {
-    var condition = "id = " + req.params.id;
-    console.log("condition", condition);
-    console.log(req.params.id);
-    burger.update({ devoured: req.body.devoured }, condition, function () {
-        res.redirect('/');
-    });
-});
+// router.put("/api/burgers/:id", function (req, res) {
+//     var condition = "id = " + req.params.id;
+//     console.log("condition", condition);
+//     console.log(req.params.id);
+//     burger.update({ devoured: req.body.devoured }, condition, function () {
+//         res.redirect('/');
+//     });
+// });
 
 module.exports = router;
